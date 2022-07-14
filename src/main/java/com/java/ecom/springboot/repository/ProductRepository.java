177fb4aa00d718aee2ba003160a3b2f3c14a6e40
nehushh <109282564+nehushh@users.noreply.github.com>
2022-07-14 22:47:28 +0530

    Add files via upload
    updating query

--- src/main/java/com/java/ecom/springboot/repository/ProductRepository.java
+++ src/main/java/com/java/ecom/springboot/repository/ProductRepository.java
@@ -9,12 +9,16 @@ import com.java.ecom.springboot.entity.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 	
-	@Query("Select p.* from products p, category c, subcategory sc where "+
+	@Query("Select p.* from products p, category c, subcategory sc,product_category pc, product_subcategory ps where "+
 			"p.productname like Concat('%', :skuSearch, '%') OR"+
 			"p.description like Concat('%', :skuSearch, '%') OR"+
 			"c.name like Concat('%', :skuSearch, '%') OR"+
 			"p.tags like Concat('%', :skuSearch, '%') OR"+
-			"sc.name like Concat('%', :skuSearch, '%')")
+			"sc.name like Concat('%', :skuSearch, '%') AND"+
+			"(p.productId=pc.productId AND"+
+			"c.categoryId = pc.categoryId) OR"+
+			"(p.productId=ps.productId AND" +
+			"sc.subcategoryid=ps.subcategoryid)")
 	List<Product> searchProducts(String skuSearch);
 
 }
